fb7f3aeb14dc5069c1faeb9a8c6d826cd45bc2ad
Houjun Tang <htang4@lbl.gov>
2021-03-08 13:45:23 -0800

    Update yml

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -4,4 +4,5 @@ set -eu -o pipefail
 
 sudo apt-get update
 
-sudo apt-get install proj-bin libhdf5-openmpi-dev python3 python3-h5py 
+sudo apt-get install proj-bin libhdf5-mpi-dev libfftw3-mpi-dev liblapack-dev
+#python3 python3-h5py 
